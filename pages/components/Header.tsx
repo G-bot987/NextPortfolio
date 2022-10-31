9edca49e572e96a9332f363d32d875777f3b8e3f
Georgia <ghobs987@gmail.com>
2022-10-31 16:40:51 +0000

    box shadow for name

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -4,7 +4,11 @@ import styles from "../../styles/Home.module.css";
 export default function Header() {
   return (
     <div className="flex-wrap">
-      <h1 className={`text-9xl italic ${styles.neonText}`}>Georgia Hobson </h1>
+      <div
+        className={`border-double border-4 border-sky-500 rounded-lg border-white min-w-min ${styles.neon}`}
+      >
+        <h1 className={`text-9xl italic ${styles.neonText}`}>Georgia Hobson</h1>
+      </div>
       <h2>Portfolio</h2>
       <p>Powered by NextJS, TypeScript and Tailwind</p>
     </div>
