d10ebb64dd5ae3c698eb592b9f1fffbb36f3c14c
Georgia <ghobs987@gmail.com>
2022-10-31 22:38:01 +0000

    header comp text align for centering and refractor on styling

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -3,14 +3,18 @@ import styles from "../../styles/Home.module.css";
 
 export default function Header() {
   return (
-    <div className="flex-wrap">
-      <div
-        className={`border-double border-4 border-sky-500 rounded-lg border-white min-w-min ${styles.neonBoxHeader}`}
-      >
-        <h1 className={`text-9xl italic ${styles.neonText}`}>Georgia Hobson</h1>
-        <h2>A Portfolio</h2>
-        <p>Powered by NextJS, TypeScript and Tailwind</p>
-      </div>
+    <div
+      className={`border-double border-4 border-sky-500 rounded-lg border-white min-w-min flex-wrap flex-col justify-center ${styles.neonBoxHeader}`}
+    >
+      <h1 className={`text-6xl p-8 italic ${styles.neonText}`}>
+        Georgia Hobson
+      </h1>
+
+      <h2 className={`italic  ${styles.neonText} `}>A Portfolio</h2>
+
+      <p className={`italic ${styles.neonText}`}>
+        Powered by NextJS, TypeScript and Tailwind
+      </p>
     </div>
   );
 }
