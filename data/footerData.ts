ba04f6056c12122f0f461401a9f07981c9d1fd4c
Georgia <ghobs987@gmail.com>
2022-10-31 20:22:13 +0000

    footer data

--- data/footerData.ts
+++ data/footerData.ts
@@ -1,3 +1,13 @@
-const footerData = { demo: "sdfsdf" };
+import linkedinIMG from "../public/images/icons/business/linkedin.png";
+import githubIMG from "../public/images/icons/business/octocat.png";
+
+const footerData = {
+  socials: "My Socials",
+  linkedin: {
+    image: linkedinIMG,
+    link: "https://www.linkedin.com/in/georgia-h-b48434150/",
+  },
+  github: { image: githubIMG, link: "https://github.com/G-bot987" },
+};
 
 export default footerData;
