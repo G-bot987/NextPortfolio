b80d507e0c98fd4228edebb29d6e93521e46d23a
Georgia <ghobs987@gmail.com>
2022-11-02 15:15:11 +0000

    syntax, fix, warning fix

--- pages/mainPages/Cv.tsx
+++ pages/mainPages/Cv.tsx
@@ -4,26 +4,28 @@ import styles from "../../styles/Home.module.css";
 // import CVlink from "../../img/cv/CV.pdf";
 export default function CV() {
   return (
-    <div className={`mt-6 mr-8 ml-8 border-double border-4 border-sky-500 rounded-lg border-white min-w-min ${styles.neonBoxBody}`}>
+    <div
+      className={`mt-6 mr-8 ml-8 border-double border-4 border-sky-500 rounded-lg border-white min-w-min ${styles.neonBoxBody}`}
+    >
       <h1>Contact Page</h1>
-      <p>
-        <h3>Proficent In:</h3>
-        <div className="HTML">HTML5</div>
-        <div className="div">CSS</div>
-        <div className="JavaScript">JavaScript</div>
-        <div className="jQuery">jQuery</div>
-        <div className="Bootstrap">Bootstrap</div>
-        <div className="Express.js">Express.js</div>
-        <div className="React.js">React.js</div>
-        <div className="Node.js ">Node.js</div>
-        <div className="Database Theory">Database Theory</div>
-        <div className="MongoDB">MongoDB</div>
-        <div className="MySQL">MySQL</div>
-        <div className="Command Line">Command Line</div>
-        <div className="Java">Javascript</div>
-        <div className="Git.">Git.</div>
+      <h3>Proficent In:</h3>
+      <div>
+        <p className="HTML">HTML5</p>
+        <p className="p">CSS</p>
+        <p className="JavaScript">JavaScript</p>
+        <p className="jQuery">jQuery</p>
+        <p className="Bootstrap">Bootstrap</p>
+        <p className="Express.js">Express.js</p>
+        <p className="React.js">React.js</p>
+        <p className="Node.js ">Node.js</p>
+        <p className="Database Theory">Database Theory</p>
+        <p className="MongoDB">MongoDB</p>
+        <p className="MySQL">MySQL</p>
+        <p className="Command Line">Command Line</p>
+        <p className="Java">Javascript</p>
+        <p className="Git.">Git.</p>
         Please click on the logo to view my CV
-      </p>
+      </div>
     </div>
   );
 }
