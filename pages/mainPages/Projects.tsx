fcb05973d5cadeb5031793718355d3cae2af3c96
Georgia <ghobs987@gmail.com>
2022-11-02 12:19:02 +0000

    removing logs

--- pages/mainPages/Projects.tsx
+++ pages/mainPages/Projects.tsx
@@ -3,9 +3,6 @@ import styles from "../../styles/Home.module.css";
 import { ProjectInterface } from "../../interface/projects.interface";
 
 export default function Projects() {
-  console.log("props in projects ");
-  // console.log(props);
-  console.log("---");
   return (
     <div
       className={`mt-6 mr-8 ml-8 border-double border-4 border-sky-500 rounded-lg border-white min-w-min ${styles.neonBoxBody}`}
