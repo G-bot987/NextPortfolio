05d93d7cc422d54ee7587d1aa8f7c9df8f1d3483
Georgia <ghobs987@gmail.com>
2022-11-02 12:18:38 +0000

    header props type checking implementing new interface

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,20 +1,19 @@
 import React from "react";
 import styles from "../../styles/Home.module.css";
+import { HeaderInterface } from "../../interface/header.interface";
 
-export default function Header() {
+export default function Header(props: HeaderInterface) {
   return (
     <div
       className={`border-double border-4 border-sky-500 rounded-lg border-white min-w-min flex-wrap flex-col justify-center ${styles.neonBoxHeader}`}
     >
       <h1 className={`text-6xl p-8 italic ${styles.neonText}`}>
-        Georgia Hobson
+        {props.mainHeader}
       </h1>
 
-      <h2 className={`italic  ${styles.neonText} `}>A Portfolio</h2>
+      <h2 className={`italic  ${styles.neonText} `}>{props.subHeader}</h2>
 
-      <p className={`italic ${styles.neonText}`}>
-        Powered by NextJS, TypeScript and Tailwind
-      </p>
+      <p className={`italic ${styles.neonText}`}>{props.technologiesTxt}</p>
     </div>
   );
 }
