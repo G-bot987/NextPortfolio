7807fe2a8bd5eaaa7bb4b3bc0f3bb1d88a39a497
Georgia <ghobs987@gmail.com>
2022-11-02 12:18:03 +0000

    remove logs

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -3,9 +3,6 @@ import styles from "../../styles/Home.module.css";
 import { FooterInterface } from "../../interface/footer.interface";
 import Image from "next/image";
 export default function Footer(props: FooterInterface) {
-  console.log("props");
-  console.log(props);
-  console.log("---");
   return (
     <div
       className={`border-solid border-b-1 border-sky-500 border-white min-w-min m-auto mr-0 my-12 bg-transparent ${styles.neonLineFooter}`}
