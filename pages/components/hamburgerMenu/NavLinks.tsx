bbc7288d237455d8db1d464921c25b40629ebf80
Georgia <ghobs987@gmail.com>
2022-11-04 17:39:20 +0000

    removing log

--- pages/components/hamburgerMenu/NavLinks.tsx
+++ pages/components/hamburgerMenu/NavLinks.tsx
@@ -60,7 +60,6 @@ function NavTabs({ currentPage, handlePageChange }: Props) {
           <a
             href="#Cv"
             onClick={() => handlePageChange("Cv")}
-            // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
             className={currentPage === "CV" ? "nav-link active" : "nav-link"}
           >
             CV
