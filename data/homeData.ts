1bced6e54dadd99c3e583c26cc02c7880d0fac34
Georgia <ghobs987@gmail.com>
2022-11-05 16:02:41 +0000

    spelling correction

--- data/homeData.ts
+++ data/homeData.ts
@@ -5,7 +5,7 @@ export const homeData = {
   synopsisProperties: [
     {
       header: "Bio",
-      txt: " Experienced Full stack web developer. competent with; NextJS, React, React.Native for IOS and Android, TypeScript, JavaScript, Node.JS, MySQL, MongoDB, Docker, IORedis, Redux, Gitbash, Sublime Merge and Deis. My current role has allowed me to develop exceptional diagnostical skills couple dwith my strong core programming princples; front end , back end and mobile development roles are well within my comfrot zone. My role as a support developer has given me experiene fixing bugs of varying complexity using technologies such as Insomnia, Redux Devtools and Android Studio.",
+      txt: " Experienced Full stack web developer. competent with; NextJS, React, React.Native for IOS and Android, TypeScript, JavaScript, Node.JS, MySQL, MongoDB, Docker, IORedis, Redux, Gitbash, Sublime Merge and Deis. My current role has allowed me to develop exceptional diagnostical skills coupled with my strong core programming princples; front end , back end and mobile development roles are well within my comfrot zone. My role as a support developer has given me experiene fixing bugs of varying complexity using technologies such as Insomnia, Redux Devtools and Android Studio.",
     },
     { header: "strengths", txt: "this is strenghts txt" },
     { header: "goals", txt: "i am goals text" },
