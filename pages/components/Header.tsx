70839848c61833e56857ef86d4e3d41cac76c579
Georgia <ghobs987@gmail.com>
2022-11-05 20:47:02 +0000

    style refractor

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -7,13 +7,11 @@ export default function Header(props: HeaderInterface) {
     <div
       className={`border-double border-4 border-sky-500 rounded-lg border-white min-w-min flex-wrap flex-col self-center my-8 ${styles.neonBoxHeader}`}
     >
-      <h1 className={`text-6xl p-8 italic ${styles.neonText}`}>
-        {props.mainHeader}
-      </h1>
+      <h1 className={`text-6xl p-8 italic`}>{props.mainHeader}</h1>
 
-      <h2 className={`italic  ${styles.neonText} `}>{props.subHeader}</h2>
+      <h2 className={`italic`}>{props.subHeader}</h2>
 
-      <p className={`italic ${styles.neonText}`}>{props.technologiesTxt}</p>
+      <p className={`italic`}>{props.technologiesTxt}</p>
     </div>
   );
 }
