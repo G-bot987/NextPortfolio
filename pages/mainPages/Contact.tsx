1957c4a80c0bfab851ed006129f5795ea235ead0
Georgia <ghobs987@gmail.com>
2022-11-05 20:53:43 +0000

    text center on contact details

--- pages/mainPages/Contact.tsx
+++ pages/mainPages/Contact.tsx
@@ -5,9 +5,11 @@ import { ContactInterface } from "../../interface/contact.interface";
 export default function Contact(props: ContactInterface) {
   return (
     <div
-      className={`mt-6 mr-8 ml-8 border-double border-4 border-sky-500 rounded-lg border-white min-w-min ${styles.neonBoxBody}`}
+      className={`mt-6 mr-8 ml-8 border-double border-4 border-sky-500 rounded-lg border-white min-w-min   ${styles.neonBoxBody}`}
     >
-      <p className={`text-base p-8 italic ${styles.neonText}`}>{props.text}</p>
+      <p className={`text-base p-8 italic text-center ${styles.neonText}`}>
+        {props.text}
+      </p>
     </div>
   );
 }
