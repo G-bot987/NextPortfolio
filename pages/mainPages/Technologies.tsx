560cfe5f55448d888c026f649c8d40e183dfcd18
Georgia <ghobs987@gmail.com>
2022-11-05 20:48:18 +0000

    refractor and animation possible commented out

--- pages/mainPages/Technologies.tsx
+++ pages/mainPages/Technologies.tsx
@@ -7,13 +7,13 @@ export default function Technologies(props: TechnologiesInterface) {
   const { skills } = props;
   return (
     <div
-      className={`mt-6 mr-8 ml-8 border-double border-4 border-sky-500 rounded-lg border-white min-w-min ${styles.neonBoxBody} text-base p-8 italic ${styles.neonText}`}
+      className={`mt-6 mr-8 ml-8 border-double border-4 border-sky-500 rounded-lg border-white min-w-min flex flex-col text-center ${styles.neonBoxBody} text-base  italic ${styles.neonText}`}
     >
-      <h3>{props.header}</h3>
-      <ul>
+      <h3 className="border-b-2 border-solid border-white">{props.header}</h3>
+      <ul className="flex-row flex justify-evenly flex-wrap">
         {skills &&
           Object.keys(skills).map((skill: any, index: number) => (
-            <div key={`${index}`}>
+            <div key={`${index}`} className="max-w-xs my-3 mx-3">
               <Skill {...skills[skill]} />
             </div>
           ))}
