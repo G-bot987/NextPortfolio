d35e9e44a452f00bd0ec031e22a39d5446b7bb8b
Georgia <ghobs987@gmail.com>
2022-11-06 22:49:46 +0000

    using obj keys and new comps for rendering

--- pages/mainPages/Home.tsx
+++ pages/mainPages/Home.tsx
@@ -1,29 +1,28 @@
 import React from "react";
-import Image from "next/image";
-
 import styles from "../../styles/Home.module.css";
 import { HomePageInterface } from "../../interface/home.interface";
-import Catagory from "../components/Catagory";
+import Bio from "../components/Bio";
+import HeaderCatagories from "../components/HeaderCatagories";
 
 export default function Home(props: HomePageInterface) {
   const { synopsisProperties } = props;
   return (
     <div
-      className={`mt-6 mr-8 ml-8 border-double border-4 border-sky-500 rounded-lg border-white min-w-min ${styles.neonBoxBody}`}
+      className={`mt-6 mr-8 ml-8 border-double border-4  rounded-lg border-white min-w-min  flex  ${styles.neonBoxBody}`}
     >
-      <Image
-        src={props.portrait?.IMG}
-        alt={props.portrait?.alt}
-        width={250}
-        height={250}
-        className="rounded-br-lg border-2"
-      />
-      <ul>
+      <ul className=" flex flex-row flex justify-evenly flex-wrap">
         {synopsisProperties &&
           Object.keys(synopsisProperties).map(
             (catagory: any, index: number) => (
               <div key={`${index}`}>
-                <Catagory {...synopsisProperties[catagory]} />
+                {synopsisProperties[catagory].portrait && (
+                 
+                    <Bio {...synopsisProperties[catagory]} />
+                
+                )}
+                {!synopsisProperties[catagory].portrait && (
+                  <HeaderCatagories {...synopsisProperties[catagory]} />
+                )}
               </div>
             )
           )}
