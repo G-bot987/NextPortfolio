e37b9874975aca1f0cc5653c8d9b1fe3cfc1f99b
Georgia <ghobs987@gmail.com>
2022-11-07 12:18:04 +0000

    on hover glow for img and repo links

--- pages/components/Project.tsx
+++ pages/components/Project.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import styles from "../../styles/Home.module.css";
 
 import { ProjectInterface } from "../../interface/projects.interface";
 
 export default function Project(props: ProjectInterface) {
+  // use state sequence for git
+  const [hover, setHover] = useState(false);
+
+  const handleMouseOver = () => {
+    setHover(true);
+  };
+  const handleMouseOut = () => {
+    setHover(false);
+  };
+  // use state sequence for img
+  const [hoverImg, setHoverImg] = useState(false);
+
+  const handleMouseOverImg = () => {
+    setHoverImg(true);
+  };
+  const handleMouseOutImg = () => {
+    setHoverImg(false);
+  };
+
   return (
     <li>
       <ul className={`text-base  italic flex flex-col text-center `}>
@@ -15,7 +34,11 @@ export default function Project(props: ProjectInterface) {
         </li>
         <li className="bg-white text-black">{props.description}</li>
         <div className="flex flex-row justify-around items-center ">
-          <li>
+          <li
+            className={hover ? `${styles.glow}` : ``}
+            onMouseOver={handleMouseOver}
+            onMouseOut={handleMouseOut}
+          >
             <a href={props.links?.repo}>
               <Image
                 src={props.imgs?.git}
@@ -25,7 +48,11 @@ export default function Project(props: ProjectInterface) {
               />
             </a>
           </li>
-          <li>
+          <li
+            className={hoverImg ? `${styles.glow}` : ``}
+            onMouseOver={handleMouseOverImg}
+            onMouseOut={handleMouseOutImg}
+          >
             <a href={props.links?.deployed}>
               <Image
                 src={props.imgs?.live}
