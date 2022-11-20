f9f9c05b368f5c565f8649a6fa5b4b4e026f958e
Georgia <ghobs987@gmail.com>
2022-11-20 05:14:10 +0000

    prop destructuring

--- pages/components/Bio.tsx
+++ pages/components/Bio.tsx
@@ -4,7 +4,7 @@ import { synopsisPropertiesInterface } from "../../interface/home.interface";
 import Image from "next/image";
 
 export default function Catagory(props: synopsisPropertiesInterface) {
-  const { header, txt, portrait } = props;
+  const { header, txt, portrait, hobbiesHeader, hobbiesTxt } = props;
 
   return (
     <li
@@ -17,14 +17,28 @@ export default function Catagory(props: synopsisPropertiesInterface) {
         height={250}
         className="sm:border-r-2 sm:border-b-2 rounded-tl-lg rounded-tr-lg sm:rounded-tr-none border-black sm:rounded-bl-lg w-full"
       />
-      <div className=" flex flex-col text-center justify-around">
+      <div className=" flex flex-col text-center justify-around ">
         <p
           className={`justify-center uppercase sm:border-double border-t-4 sm:border-r-4  sm:rounded-tr-lg border-white ${styles.neonText}`}
         >
           {header}
         </p>
-        <div className="grow sm:text-white text-justify flex flex-col justify-center px-4 sm:rounded-br-lg sm:border-y-4  border-r-4 border-double  sm:bg-transparent bg-white text-black ">
-          <p>{txt}</p>
+        <div className="grow sm:text-white text-justify flex flex-col justify-center  sm:rounded-br-lg sm:border-y-4  border-r-4 border-double  text-black w-full">
+          <div className="px-4 flex flex-col text-justify sm:bg-transparent bg-white ">
+            <p>{txt}</p>
+          </div>
+          <div className=" border-t-2 border-white sm:text-white flex flex-col sm:bg-transparent text-black  w-full">
+            <div className="border-b-2 border-t-2 sm:border-t-0 sm:border-white border-black uppercase">
+              <p
+                className={`justify-center uppercase  text-center ${styles.neonText}`}
+              >
+                {hobbiesHeader}
+              </p>
+            </div>
+            <div className="px-4 flex flex-col text-justify sm:bg-transparent bg-white ">
+              <p>{hobbiesTxt}</p>
+            </div>
+          </div>
         </div>
       </div>
     </li>
