d2db9ea3d5d7a6b2c4675ae60d1a51f90ac18076
gbot987 <ghobs987@gmail.com>
2023-01-29 21:30:59 +0000

    conditional caro slide render

--- pages/components/CaroSlides.tsx
+++ pages/components/CaroSlides.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
-import { slideInterface } from '../../interface/home.interface'
+import { CarouselSlideInterface } from '../../interface/carousel.interface'
 import Image from 'next/image';
-import { imageConfigDefault } from 'next/dist/shared/lib/image-config';
 
-export default function CaroSlides(props: slideInterface) {
-    const { img, alt } = props
-
+export default function CaroSlides(props: CarouselSlideInterface) {
+    const { type, img, alt, experience, mainHeader, subHeader, technologiesTxt } = props
     return (
-        <li>
-            <Image src={img}
-                alt={alt}
-                width={700}
-                height={700}
-            />
+        <li className='items-center flex flex-col min-w-full min-h-full'>
+            {type === 'img' && img && alt &&
+                <div>
+                    <h2 className='text-2xl	'>{experience}</h2>
+                    <Image src={img}
+                        alt={alt}
+                        width={500}
+                        height={500}
+                    />
+                </div>
+            }
+
+            {
+                type === 'txt' &&
+                <div className='min-w-full min-h-full'>
+
+                    <h1 className={`text-6xl p-8 italic`}>{mainHeader}</h1>
+
+                    <h2 className={`italic`}>{subHeader}</h2>
+
+                    <p className={`italic`}>{technologiesTxt}</p>
+                </div>
+
+            }
         </li>
     )
 }
