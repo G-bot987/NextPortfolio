11ad0fe345ce4953cbd97a664293a2b28a7eb8c2
gbot987 <ghobs987@gmail.com>
2023-01-29 01:45:46 +0000

    working time out function

--- pages/mainPages/Home.tsx
+++ pages/mainPages/Home.tsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../../styles/Home.module.css";
 import { HomePageInterface } from "../../interface/home.interface";
 import Bio from "../components/Bio";
 import HeaderCatagories from "../components/HeaderCatagories";
+import CaroSlides from "../components/CaroSlides";
 
 export default function Home(props: HomePageInterface) {
-  const { synopsisProperties } = props;
+  const { synopsisProperties, carouselSlides } = props;
+
+  const slideArrLength = carouselSlides.length
+  var [slide, SetSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => SetSlide(slide = slide + 1), 10000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [])
+
   return (
-    <div className={`mt-6 mr-8 ml-8  min-w-min  flex`}>
+    <div className={`mt-6 mr-8 ml-8  min-w-min  flex flex-col`}>
+      {/* <CaroSlides {...carouselSlides} /> */}
       <ul className=" flex sm:flex-row justify-evenly flex-wrap border-double border-4 border-white rounded-lg sm:border-0  ">
         {synopsisProperties &&
           Object.keys(synopsisProperties).map(
