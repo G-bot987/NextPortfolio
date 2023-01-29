914e1bc9d4f777ec3a1c26476975bd56abb20f88
gbot987 <ghobs987@gmail.com>
2023-01-29 13:45:09 +0000

    uniformed sizing of imgs

--- pages/components/CaroSlides.tsx
+++ pages/components/CaroSlides.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { slideInterface } from '../../interface/home.interface'
 import Image from 'next/image';
+import { imageConfigDefault } from 'next/dist/shared/lib/image-config';
 
 export default function CaroSlides(props: slideInterface) {
+    const { img, alt } = props
 
     return (
         <div>
 
-            <Image src={props.img}
-                alt=''
+            <Image src={img}
+                alt={alt}
+                width={700}
+                height={700}
             />
         </div>
     )
