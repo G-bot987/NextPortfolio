1367be9653de89d28b9b420b94c6b0313de72382
gbot987 <ghobs987@gmail.com>
2023-02-02 01:28:53 +0000

    build fix

--- pages/mainPages/Projects.tsx
+++ pages/mainPages/Projects.tsx
@@ -11,7 +11,7 @@ export default function Projects(props: ProjectsDataInterface) {
       className={`mt-6 mr-8 ml-8 border-double border-4 rounded-lg border-white min-w-min  flex-row`}
     >
       <ul className={`flex-row flex justify-evenly flex-wrap`}>
-        {projects.map((project: any, index: number) => (
+        {projects?.map((project: any, index: number) => (
           <Project {...project} key={`${index}`} />
         ))}
       </ul>
