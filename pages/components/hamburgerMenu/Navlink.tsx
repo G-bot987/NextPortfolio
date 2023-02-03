b38a51475c7812003bf9bae3bb8ce3c20b8f8204
gbot987 <ghobs987@gmail.com>
2023-02-03 19:58:00 +0000

    fix for cv tab functionality and styling for navtabs, using comp for tabs to stop repitition

--- pages/components/hamburgerMenu/Navlink.tsx
+++ pages/components/hamburgerMenu/Navlink.tsx
@@ -1,33 +1,47 @@
 import React, { useState } from 'react'
+import Link from "next/link";
+
+
 
 export default function NavLink(Props: any) {
-    const { link, handlePageChange, currentPage } = Props
-    console.log('link')
-    console.log(link)
-    console.log('---')
-
-
-
+    const { link, handlePageChange, currentPage, index } = Props
     const [hover, setHover] = useState(false);
 
 
     const handleMouseOver = () => {
-        setHover(!hover);
+        setHover(true);
     };
     const handleMouseOut = () => {
-        setHover(!hover);
+        setHover(false);
     };
+
     return (
-        <li className="pl-4  bg-black rounded-tl-lg rounded-bl-lg"
-            onMouseOver={handleMouseOver}
-            onMouseOut={handleMouseOut}
+        <li className={`px-2  bg-black ${index === 0 ? `rounded-tl-lg rounded-bl-lg` : ``} ${index === 4 ? `rounded-tr-lg rounded-br-lg` : ``}  ${hover ? `bg-sky-500` : ``}`}
         >
-            <a
+
+            {link !== 'CV' && <a
                 href={`#${link}`}
                 onClick={() => handlePageChange(`${link}`)}
                 className={currentPage === `${link}` ? "nav-link active" : "nav-link"}
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
+
             >
                 {link}
-            </a>
+            </a>}
+            {link === 'CV' &&
+                <Link href={"images/CV/CV.pdf"} legacyBehavior>
+                    <a
+                        href="#Cv"
+                        onClick={() => handlePageChange("Cv")}
+                        className={currentPage === "CV" ? "nav-link active" : "nav-link"}
+                        onMouseOver={handleMouseOver}
+                        onMouseOut={handleMouseOut}
+                    >
+                        CV
+                    </a>
+
+                </Link>
+            }
         </li>)
 }
