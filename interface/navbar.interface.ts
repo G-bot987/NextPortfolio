19f21fe30d368ef603b37bca4b9da09f422e7295
gbot987 <ghobs987@gmail.com>
2023-02-12 21:01:41 +0000

    new interfaces

---
+++ interface/navbar.interface.ts
@@ -0,0 +1,12 @@
+export interface NavlinksInterface {
+    NavbarData: String[],
+    currentPage: string;
+    handlePageChange: any;
+}
+
+export interface NavLinkInterface {
+    link: string,
+    index: number,
+    currentPage: string,
+    handlePageChange: any
+}
