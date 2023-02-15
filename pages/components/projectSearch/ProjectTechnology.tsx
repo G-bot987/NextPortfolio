416925ece236970e86823ca2340e12fa061db109
gbot987 <ghobs987@gmail.com>
2023-02-15 22:20:09 +0000

    updating redux store with selected tec

--- pages/components/projectSearch/ProjectTechnology.tsx
+++ pages/components/projectSearch/ProjectTechnology.tsx
@@ -1,20 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { TechnologyInterface } from '../../../interface/projects.interface'
+import { choosenTech, removeTech } from '../../../slices/projectSlice'
 
 export default function ProjectTechnology(props: any) {
     const [select, SetSelect] = useState(false)
 
-    console.log('tec props')
-    console.log(props)
-    console.log('---')
+    const dispatch = useDispatch();
+
+    const { technology } = props
+    const { tec, key } = technology
+
+    useEffect(() => {
+        const reduxPayload = key
+        if (select) {
+            dispatch(choosenTech(reduxPayload))
+        } else {
+            dispatch(removeTech(reduxPayload))
+        }
+    }, [select])
+
     return (
         <li >
             <button className="max-w-fit rounded-full border-2 border-white p-2 my-2"
+                onClick={() => SetSelect(!select)}
                 style={{
                     background: select ? "white" : "",
                     color: select ? "black" : "",
                     border: select ? "solid" : "",
                 }}>
-                {props.tec}
+                {tec}
             </button>
         </li>
     )
