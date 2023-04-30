c3c8e66fef5a7461aa98d669cc82f0b152fa68f9
gbot987 <ghobs987@gmail.com>
2023-04-30 08:59:15 +0100

    removing margin

--- pages/components/HeaderCatagories.tsx
+++ pages/components/HeaderCatagories.tsx
@@ -6,7 +6,7 @@ import { synopsisPropertiesInterface } from "../../interface/home.interface";
 export default function HeaderCatagories(props: synopsisPropertiesInterface) {
   const { header, txt } = props;
   return (
-    <li className={`italic text-center  max-w-xs list-none mx-12 `}>
+    <li className={`italic text-center  max-w-xs list-none max-[602px]:mx-12 `}>
       <div className="text-center flex flex-col flex-shrink  min-[602px]:rounded-b-lg min-[602px]:rounded-t-none bg-white p-7 text-black rounded-full  min-[700px]:max-h-[600px] min-[1100px]:max-h-[400px]">
         <p className="text-bold underline font-bold">{header}</p>
         <p>{txt}</p>
