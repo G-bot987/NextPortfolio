f8998b20a1e7ac871b42de70f5566d49568afb82
gbot987 <ghobs987@gmail.com>
2023-07-07 02:30:35 +0100

    updated project data for project search option

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -17,18 +17,18 @@ export const projectsData = {
 
 
   technologies: [{ key: 1, option: 'react' }, { key: 2, option: 'next' }, { key: 3, option: 'mongodb' }, { key: 4, option: 'javascript' }, { key: 5, option: 'typescript' }, { key: 6, option: 'redux' }, { key: 7, option: 'node' }, { key: 8, option: 'mysql' }, { key: 9, option: 'tailwind' }, { key: 10, option: 'express' }, { key: 11, option: 'sequelize' }, 
-   { key: 13, option: 'SCSS' },  { key: 15, option: 'GraphQL' }, {key:16, option: 'PHP'}, {key:17, option: 'Laravel'}, {key:18, option: 'heroku'}, {key:19, option: 'bootstrap'}, { key: 26, option: 'Shopify' }],
+   { key: 13, option: 'SCSS' },  { key: 15, option: 'GraphQL' }, {key:16, option: 'PHP'}, {key:17, option: 'Laravel'}, {key:18, option: 'heroku'}, {key:19, option: 'bootstrap'}, { key: 26, option: 'Shopify' }, {key:30, option: 'vercel'}],
 
   recentProject: [{key: 20, option: 'Yes'}],
 
-  projectType:[{ key: 21, option: 'Front End' }, { key: 22, option: 'Back End' }, { key: 23, option: 'Full Stack' }, { key: 24, option: 'UX' }, { key: 25, option: 'Built from Figma Design' }, { key: 14, option: 'API' }, { key: 12, option: 'Tec Test' }, { key: 27, option: 'Passion Project' }, { key: 28, option: 'Hosted' }, { key: 29, option: 'Demo Video' }],
+  projectType:[{ key: 21, option: 'Front End' }, { key: 22, option: 'Back End' }, { key: 23, option: 'Full Stack' }, { key: 24, option: 'UX' }, { key: 25, option: 'Built from Figma Design' }, { key: 14, option: 'API' }, { key: 12, option: 'Tec Test' }, { key: 27, option: 'Passion Project' }, { key: 28, option: 'Hosted' }, { key: 29, option: 'Demo Video' }, {key: 31 , option:'colaborative project'}, {key: 32, option: 'BEM'}],
 
   projects: [
     {
       
       header: "DnD Character Manager",
       showCase: true,
-      technologies: [2, 3, 5, 6, 9],
+      technologies: [2, 3, 5, 6, 9, 27, 23, 14, 28, 20, 30],
       description:
         "Using a Dungeons and Dragons 5E character card as a template, this application offers a intuitive interface for player character creation. This project is currently in production but will be integrated with a backend for storing and managing user data.",
       links: {
@@ -48,7 +48,7 @@ export const projectsData = {
       
       header: "Pokedex",
       showCase: false,
-      technologies: [12, 14, 16, 17, 18,19, 8],
+      technologies: [12, 14, 16, 17, 18, 19, 8, 28, 12, 14, 23, 20],
       description:
         "My first Laravel application with a MySQL database for storing user and pokemon data. This application demonstrates; Authentication, route guarding, API usuage specifically create and read, database storage and data filtering. This project directs users to a login page after creating a user they can then log in a view a Pokedex with all pokemon data. Users can then view specific Pokemon and search for Pokemon based on type. Future iterations will have a more sophisticated filter functionality as well as the ability to favourite pokemon and add private notes to specific Pokemon",
       links: {
@@ -67,7 +67,7 @@ export const projectsData = {
       
       header: "Autism Society, Front End Tec Test Showcase",
       showCase: true,
-      technologies: [2, 5, 12, 13],
+      technologies: [2, 5, 12, 13, 30, 20, 21, 24, 25, 28, 32],
       description:
         "A demonstration of my front end work taken from a figma using BEM methodology and SCSS",
       links: {
@@ -88,7 +88,7 @@ export const projectsData = {
       
       header: "Shopify Tec Test, A GraphQL API Showcase",
       showCase: true,
-      technologies: [5, 7,12, 14, 15],
+      technologies: [5, 7,12, 14, 15, 20, 22,29, 26],
       description:
         "Two Scripts one for reading data from a Shopify store and another for mutating the stores data",
       links: {
@@ -107,7 +107,7 @@ export const projectsData = {
 
     {
       header: "Tech Blog",
-      technologies: [4, 7, 11, 8, 18],
+      technologies: [4, 7, 11, 8, 18, 28, 23],
       showCase: false,
       description:
         "A difficult and time consuming project, with both a working front and backend and login logic using, sequlizes, handlebars, cookies and api calls",
@@ -124,7 +124,7 @@ export const projectsData = {
     },
     {
       header: "5StarLocal Group Project",
-      technologies: [4],
+      technologies: [4, 31, 14,23],
       showCase: false,
       description: "A group project with  similar functionality to TechBlog",
       links: {
@@ -141,7 +141,7 @@ export const projectsData = {
     },
     {
       header: "Employee Manager",
-      technologies: [4, 7],
+      technologies: [4, 7, 29, 22],
       showCase: false,
       description:
         "A backend commandline application with employee management application",
@@ -159,7 +159,7 @@ export const projectsData = {
     },
     {
       header: "Inquistive Group Project",
-      technologies: [1, 4, 9, 3],
+      technologies: [1, 4, 9, 3,31,14,23],
       showCase: false,
       description:
         "The most complicated application that I have worked on, A full stack quiz application",
@@ -177,7 +177,7 @@ export const projectsData = {
     },
     {
       header: "Password Generator",
-      technologies: [4, 7],
+      technologies: [4, 7,28],
       showCase: false,
       description: "A javascript password generator, that works to user criteria",
       links: {
@@ -194,7 +194,7 @@ export const projectsData = {
     },
     {
       header: "Weatherdashboard",
-      technologies: [4, 7],
+      technologies: [4, 7,14,28],
       showCase: false,
       description:
         "Shows you weather conditions for a selected location, using api calls",
