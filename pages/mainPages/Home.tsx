db61d37a843d8854d4c3b565088f8b4e3af41e1c
gbot987 <ghobs987@gmail.com>
2023-07-18 21:48:21 +0100

    landing pg responsive borders and alignement with img stop over lap

--- pages/mainPages/Home.tsx
+++ pages/mainPages/Home.tsx
@@ -10,7 +10,7 @@ export default function Home(props: HomePageInterface) {
 
   return (
     <div className="mt-6 mr-8 ml-8  min-w-min  flex flex-col items-center min-h-screen	mb-8">
-      <ul className=" flex  justify-evenly flex-wrap border  rounded-lg sm:border-0  ">
+      <ul className=" flex  justify-evenly flex-wrap   ">
         {synopsisProperties && (
           <div>
             <Bio {...synopsisProperties[0]} />
