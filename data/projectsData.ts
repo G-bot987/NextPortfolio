25ae2a1dede5c3ca0f382202052f515876fbb199
gbot987 <ghobs987@gmail.com>
2023-07-19 22:01:28 +0100

    improved project data and new interface

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -29,8 +29,8 @@ export const projectsData = {
       header: "DnD Character Manager",
       showCase: true,
       technologies: [2, 3, 5, 6, 9, 27, 23, 14, 28, 20, 30],
-      description:
-        "Using a Dungeons and Dragons 5E character card as a template, this application offers a intuitive interface for player character creation. This project is currently in production but will be integrated with a backend for storing and managing user data.",
+      summary: "Using a Dungeons and Dragons 5E character card as a template, this application offers a intuitive interface for player character creation.",
+      description: "In development since late November 22, if you check out the README, you will see the plan regarding future development. This project stems from my passion for tabletop games and the challenges of running games during the Covid pandemic. Based on my personal experience with platforms like Roll20 and other digital tabletops, I found the user interface and overall design to be generic. The platform Ive been developing takes a more tailored approach to character creation, incorporating languages from my homebrew world, for example. I undertook this project to push myself out of my comfort zone and tackle complex state management issues, UX considerations, database design, and gain more exposure to backend/API development. I specifically chose Next.js instead of React to gain more experience with Next.js and leverage its optimization benefits over React, such as the use of getServerSideProps. MongoDB was selected partly because of its guaranteed free tier and also due to its seamless integration with Next.js. TypeScript offers superior bug prevention compared to JavaScript, and its interface functionality aligns well with a prop-based framework, as well as providing reassurance when dealing with APIs.",
       links: {
         deployed: "https://next-dn-d-character-manager.vercel.app/",
         repo: "https://github.com/G-bot987/NextDnDCharacterManager",
@@ -51,9 +51,11 @@ export const projectsData = {
       header: "Pokedex",
       showCase: false,
       technologies: [12, 14, 16, 17, 18, 19, 8, 28, 12, 14, 23, 20],
-      description:
-        "My first Laravel application with a MySQL database for storing user and pokemon data. This application demonstrates; Authentication, route guarding, API usuage specifically create and read, database storage and data filtering. This project directs users to a login page after creating a user they can then log in a view a Pokedex with all pokemon data. Users can then view specific Pokemon and search for Pokemon based on type. Future iterations will have a more sophisticated filter functionality as well as the ability to favourite pokemon and add private notes to specific Pokemon",
-      links: {
+      summary:
+        "This project marks my first foray into Laravel and PHP development. It served as a technical test for my current role. Despite having no prior experience with PHP, I dedicated one week to complete this project, showcasing my aptitude and eagerness to explore new areas of development. I am pleased with the result I achieved within such a short timeframe!",
+        description: "This application showcases various functionalities, including authentication, route guarding, API usage (specifically for creating and reading data), database storage, and data filtering. Upon accessing the application, users are directed to a login page. After creating a user account, they can log in and access a Pokedex containing comprehensive Pokemon data. Users have the ability to view specific Pokemon and search for Pokemon based on their types. In future iterations, the application will include more advanced filtering options and additional features such as favoriting Pokemon and adding private notes to specific Pokemon.",
+
+        links: {
         deployed: "http://alluring-kings-canyon-06696-e97f27ca535a.herokuapp.com",
         repo: "https://github.com/G-bot987/Pokedex",
         deployedTxt: 'deployment',
@@ -72,9 +74,10 @@ export const projectsData = {
       header: "Autism Society, Front End Tec Test Showcase",
       showCase: true,
       technologies: [2, 5, 12, 13, 30, 20, 21, 24, 25, 28, 32],
-      description:
-        "A demonstration of my front end work taken from a Figma using SCSS, the BEM Methodoly and built to AA accessibility standards",
-      links: {
+      summary:
+        "A demonstration of my front end work using a Figma Design as reference, built using the BEM Methodoly and to AA accessibility standards",
+        description: "This project highlights my proficiency in translating high-quality designs into pixel-perfect representations. It is designed with a mobile-first approach, showcasing my meticulous attention to detail. I have incorporated subtle yet effective elements such as animations and hover effects to enhance the user experience. While this project primarily focuses on front-end development, I have also utilized TypeScript and prop passing to render data in a manner that demonstrates the successful integration of a headless CMS through API requests. This showcases not only the visual aspect but also the behavior of a front-end application after receiving data from the CMS.",
+        links: {
         deployed: "https://adaptable-three.vercel.app/",
         repo: "https://github.com/G-bot987/adaptable",
         deployedTxt: 'deployment',
@@ -94,9 +97,9 @@ export const projectsData = {
       header: "Shopify Tec Test, A GraphQL API Showcase",
       showCase: true,
       technologies: [5, 7,12, 14, 15, 20, 22,29, 26],
-      description:
-        "Two Scripts one for reading data from a Shopify store and another for mutating the stores data",
-      links: {
+      summary:"Two Scripts one for reading data from a Shopify store and another for mutating the stores data",
+      description: "This project demonstrates the process of compiling and executing TypeScript in the command line, utilizing interaction with a Shopify store through a GraphQL API. The first script focuses on performing basic mathematical calculations, such as determining sales per item and calculating the total sale value. These calculations provide valuable data for store owners or users engaged in basic marketing operations. The second script takes the compiled data from the first script and utilizes a GraphQL mutation to send the data back to the Shopify store, thereby updating the store's data. This ensures that the store remains up to date and displays relevant marketing information for enhanced user experience and ease of use. By working on this project, I aimed to showcase my proficiency in leveraging TypeScript, interacting with external APIs, and manipulating data to provide actionable insights. It highlights my ability to integrate with third-party platforms and deliver practical solutions that benefit businesses and store owners.",
+        links: {
         deployed: "https://drive.google.com/file/d/15txAIc4Z2BPdBKAf3sJxDxTj62HhlTK2/view",
         repo: "https://github.com/G-bot987/cake",
         deployedTxt: 'Demo Video',
@@ -115,8 +118,8 @@ export const projectsData = {
       header: "Tech Blog",
       technologies: [4, 7, 11, 8, 18, 28, 23],
       showCase: false,
-      description:
-        "A difficult and time consuming project, with both a working front and backend and login logic using, sequlizes, handlebars, cookies and api calls",
+      summary: "completed whilst studying for my certificate at Birmingham University. At the time this full stack application daunted me and pushed me to my limits, its crazy how far I've come. Sometimes I just look at this code to see my progress.",
+      description: "This application demonstrates the usage of JWT for login functionality, route guarding, and API integration for CRUD operations. It allows users to log in, create posts, comment on posts from other users, as well as edit and delete their own posts or comments. At the time of development, my focus was primarily on getting the functionality working, so I didn't pay as much attention to the user experience (UX). If you're looking for a front-end showcase, I recommend checking out this portfolio or my Autism Society tech test, which are great starting points.",
       links: {
         deployed: "https://dry-eyrie-85153.herokuapp.com/",
         repo: "https://github.com/G-bot987/TechBlog",
@@ -132,9 +135,10 @@ export const projectsData = {
     },
     {
       header: "5 Star Local Group Project",
-      technologies: [4, 31, 14,23],
+      technologies: [4, 31, 14,23, 19, 8],
       showCase: false,
-      description: "A group project with  similar functionality to TechBlog",
+      summary: "A group project from my time at Birmingham University a colaborative take on my Tech Blog project",
+      description: "my 2nd stage group assignment piece from Birmingham. A full stack application that allows users to log in create and review restraunts. One of earlier projects illustrates colaboration on git conflict resolution and peer review.",
       links: {
         repo: "https://github.com/G-bot987/My-5-Star-Local",
         deployedTxt: 'No app',
@@ -152,8 +156,8 @@ export const projectsData = {
       header: "Employee Manager",
       technologies: [4, 7, 29, 22],
       showCase: false,
-      description:
-        "A backend commandline application with employee management application",
+      summary: "Commandline application for employee management",
+      description: "This Node application enables users to efficiently manage and create employee accounts while providing access to department and salary information. Developed during my time at Birmingham University, this project serves as a showcase for the unique skills and experience that may not be evident in my other projects. The application allows users to perform various actions, such as creating and managing employee accounts, as well as gaining insights into departmental structures and salary details. It demonstrates my proficiency in building robust back-end systems and showcases the practical application of my skills within a real-world scenario.  By developing this project, I aimed to further enhance my capabilities and highlight my ability to tackle complex tasks. It serves as a testament to my dedication, problem-solving skills, and commitment to delivering high-quality software solutions.",
       links: {
         deployed:
           "https://drive.google.com/file/d/16ZVj7zfFe_IYRHH2Utjb4Rl4dU076VWi/view",
@@ -172,8 +176,8 @@ export const projectsData = {
       header: "Inquistive Group Project",
       technologies: [1, 4, 9, 3,31,14,23],
       showCase: false,
-      description:
-        "The most complicated application that I have worked on, A full stack quiz application",
+      summary:"The most complicated application that I have worked on, A full stack quiz application",
+      description: "This collaborative project, developed during my final stage at Birmingham, showcases various key features. It implements route guarding, JWT interaction, and login functionality using React's state management, resulting in an intuitive user experience. The application retrieves quiz data from an API based on the user's selected parameters. Once a user has chosen a quiz, it is stored in a database for future access and associated with that specific user. Other users can then log in and participate in the quiz. The primary objective of this project was to address the social isolation experienced during the Covid pandemic. The platform offers an interactive alternative to traditional pub quizzes, providing a digital activity for groups of friends to enjoy together. By working on this project, I aimed to create a solution that fosters connection and engagement among people, enabling them to have enjoyable experiences despite physical distancing measures. It highlights my ability to design and develop applications that cater to specific user needs and promote social interaction. ",
       links: {
         repo: "https://github.com/G-bot987/inQuizitive",
         deployedTxt: 'No app',
@@ -190,7 +194,8 @@ export const projectsData = {
       header: "Password Generator",
       technologies: [4, 7,28],
       showCase: false,
-      description: "A javascript password generator, that works to user criteria",
+      summary: "A javascript password generator, that works to user criteria. Developed whilst I was at Birmingham University",
+      description: "This project demonstrates the utilization of JavaScript to interact with user input and generate secure passwords based on various parameters. The application allows users to customize their password generation by selecting options such as special characters, letters, capitalization, and more. By leveraging JavaScript, the application dynamically generates passwords that meet the specified criteria, ensuring a high level of security. Users have control over the complexity of their passwords, tailoring them to their individual preferences and security needs. This project highlights the versatility and power of JavaScript in providing a seamless and interactive user experience. It showcases the ability to handle user input, process it, and generate outputs that adhere to specific requirements. By offering this password generation functionality, the application contributes to promoting strong password practices and enhancing overall online security.",
       links: {
         deployed: "https://g-bot987.github.io/20.06.21-password-generator/",
         repo: "https://github.com/G-bot987/20.06.21",
@@ -208,8 +213,8 @@ export const projectsData = {
       header: "Weather dashboard",
       technologies: [4, 7,14,28],
       showCase: false,
-      description:
-        "Shows you weather conditions for a selected location, using api calls",
+      summary:"Shows you weather conditions for a selected location, using api calls.",
+      description: "This project demonstrates the interaction between a JavaScript application and an API to retrieve and render weather data. By utilizing form data, the application allows users to input their desired location or search criteria to obtain relevant weather information. Through API integration, the application sends the user's input to the weather data API, retrieving the corresponding weather data. The retrieved data is then dynamically rendered in the application, providing the user with up-to-date and accurate weather information. This project showcases the seamless interaction between JavaScript and APIs, illustrating how form data can be utilized to interact with external data sources. By leveraging this functionality, the application empowers users to access and visualize weather data specific to their preferences or desired locations. The project serves as an example of how JavaScript applications can leverage APIs to enhance user experiences by providing real-time and personalized information. It highlights the versatility of JavaScript in handling user input, processing data, and presenting it in a meaningful way.",
       links: {
         deployed: "https://g-bot987.github.io/weatherdashboard/",
         repo: "https://github.com/G-bot987/weatherdashboard",
