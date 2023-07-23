e6e32100aa2149b0a5ab00838651b5a107b5ca1b
gbot987 <ghobs987@gmail.com>
2023-07-23 20:05:02 +0100

    basic render

--- pages/components/homePgComps/experienceComps/Employer.tsx
+++ pages/components/homePgComps/experienceComps/Employer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { experienceInterface } from "../../../../interface/home.interface";
+import Technology from "./Technology";
 
 interface RoleInterface {
   job: experienceInterface;
@@ -8,15 +9,37 @@ interface RoleInterface {
 
 export default function Employer(props: RoleInterface) {
   const { job, index } = props ?? {};
-  console.log(index);
-  const { company } = job ?? {};
+  const { company, dates, leave, jobtitle, tec, description } = job ?? {};
 
   const evenIndex = index % 2 === 0 ? true : false;
 
   console.log(evenIndex);
   return (
-    <article className={`max-w-xs   ${evenIndex ? ` self-start` : `self-end`}`}>
-      {company} {index}
+    <article
+      className={`  bg-white  text-black	flex flex-col min-w-[50%] ${
+        evenIndex ? ` self-start rounded-r-lg` : `self-end rounded-l-lg`
+      }`}
+    >
+      <section className="flex flex-row min-w-full">
+        <section className="flex flex-row min-w-full justify-between">
+          <section className="flex flex-col">
+            <h2>{company}</h2>
+            <h3>{jobtitle}</h3>
+          </section>
+          <section className="flex flex-col justify-self-end">
+            <h3>{dates}</h3>
+            <h3>{leave}</h3>
+          </section>
+        </section>
+      </section>
+      <section>
+        <ul className="flex flex-row">
+          {tec.map((techology: string, index: number) => (
+            <Technology key={index} techology={techology} />
+          ))}
+        </ul>
+        {description}
+      </section>
     </article>
   );
 }
