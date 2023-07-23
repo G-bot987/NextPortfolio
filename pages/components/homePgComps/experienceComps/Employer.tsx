c74e25d1805f5e8e8364d2492b8a0fa5579df753
gbot987 <ghobs987@gmail.com>
2023-07-23 22:28:47 +0100

    updated styling and ux for experience section fonts and formatting

--- pages/components/homePgComps/experienceComps/Employer.tsx
+++ pages/components/homePgComps/experienceComps/Employer.tsx
@@ -9,39 +9,52 @@ interface RoleInterface {
 
 export default function Employer(props: RoleInterface) {
   const { job, index } = props ?? {};
-  const { company, dates, leave, jobtitle, stack, description } = job ?? {};
+  const { company, dates, leave, jobtitle, stack, role } = job ?? {};
   const { header, body } = leave ?? {};
-  console.log(props);
 
   const evenIndex = index % 2 === 0 ? true : false;
 
-  console.log(evenIndex);
+  const companyTxt = Object.keys(job || {})[0] || "";
+  const stackTxt = Object.keys(job || {})[4] || "";
+  const roleTxt = Object.keys(job || {})[5] || "";
+  console.log(roleTxt);
+
   return (
     <article
-      className={`  bg-white  text-black	flex flex-col min-w-[50%] ${
+      className={`  bg-white  text-black	flex flex-col min-w-[50%] font-bd-retrocentric p-4 space-y-2 ${
         evenIndex ? ` self-start rounded-r-lg` : `self-end rounded-l-lg`
       }`}
     >
       <section className="flex flex-row min-w-full">
         <section className="flex flex-row min-w-full justify-between">
-          <section className="flex flex-col">
-            <h2>{company}</h2>
+          <section className="flex flex-col uppercase space-y-2">
+            <h2>
+              {` ${companyTxt} :
+              ${company}`}
+            </h2>
             <h3>{jobtitle}</h3>
           </section>
-          <section className="flex flex-col justify-self-end">
-            <h3>{dates}</h3>
-            <h3>{body}</h3>
+          <section className="flex flex-col justify-self-end space-y-2">
+            <h3 className="uppercase">{dates}</h3>
+            <article className="flex flex-row space-x-2">
+              <h3 className="uppercase ">{`${header}:`}</h3>
+              <p className="font-tektur">{body}</p>
+            </article>
           </section>
         </section>
       </section>
-      <section>
-        <ul className="flex flex-row">
+      <section className="space-y-2">
+        <ul className="flex flex-row justify-start uppercase">
+          <li>{`${stackTxt} :`}</li>
           {stack &&
             stack.map((techology: string, index: number) => (
               <Technology key={index} techology={techology} />
             ))}
         </ul>
-        {description}
+        <article className="flex flex-row justify-start">
+          <h3 className="font-bd-retrocentric uppercase"> {`${roleTxt} :`}</h3>
+          <p className="font-tektur">{role}</p>
+        </article>
       </section>
     </article>
   );
