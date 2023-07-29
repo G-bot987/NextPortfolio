baa5b5ebe965cc01dbdda76a06a7e78e670c2665
gbot987 <ghobs987@gmail.com>
2023-07-30 00:10:04 +0100

    updating interface and data for tecs pg

--- interface/technologies.interface.ts
+++ interface/technologies.interface.ts
@@ -1,8 +1,15 @@
 export interface SkillInterface {
   name: string;
+  skillGroupKey: number;
 }
 
 export interface TechnologiesInterface {
   header: string;
   skills: SkillInterface[];
+  skillgroups: skillGroupInterface[];
+}
+
+export interface skillGroupInterface {
+  key: number;
+  groupName: string;
 }
