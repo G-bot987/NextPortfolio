8d95fdeb17c4a41e4f2374d2436eceedf28e8714
gbot987 <ghobs987@gmail.com>
2023-07-30 20:55:12 +0100

    search tec functionality using fuse

--- pages/mainPages/Technologies.tsx
+++ pages/mainPages/Technologies.tsx
@@ -1,23 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Fuse from "fuse.js";
 import styles from "../../styles/Home.module.css";
-import { TechnologiesInterface } from "../../interface/technologies.interface";
+import {
+  SkillInterface,
+  TechnologiesInterface,
+  skillGroupInterface,
+  fuseSkillInterface,
+} from "../../interface/technologies.interface";
 import Skill from "../components/skillPgComps/Skill";
 
 export default function Technologies(props: TechnologiesInterface) {
-  const { skills } = props;
+  const { skills, header, skillgroups } = props;
+
+  const [tecSearchInput, setTecSearchInput]: any = useState("");
+  const [renderSkill, setRenderSKill]: any = useState([]);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTecSearchInput(event.target.value);
+    filterTechnology(event.target.value);
+  };
+
+  const filterTechnology = (tecSearchInput: string) => {
+    const options = {
+      includeScore: true,
+      keys: ["name"],
+    };
+    const fuse = new Fuse(skills, options);
+    const foundSkills = fuse.search(`${tecSearchInput}`, { limit: 3 });
+
+    setRenderSKill(foundSkills);
+  };
+
   return (
-    <section
-      className={`mt-6 mr-8 ml-8 border-double border-4  rounded-lg border-white min-w-min flex flex-col text-center  text-base`}
-    >
-      <h3
-        className={`border-b-2 border-solid border-white font-bd-retrocentric uppercase ${styles.neonText}`}
-      >
-        {props.header}
-      </h3>
+    <section className="flex flex-col text-white font-tektur">
+      <ul className="flex flex-col items-center max-md:hidden ">
+        <h2 className="font-bd-retrocentric uppercase underline">{header}</h2>
+        <li className="flex flex-row"></li>
+        <li className="max-w-fit rounded-full border-2 border-white p-2 my-2 uppercase">
+          <label htmlFor="tec-search">Skill Search:</label>
+          <input
+            type="search"
+            id="tec-search"
+            name="tec"
+            value={tecSearchInput}
+            onChange={handleInputChange}
+          />
+        </li>
+      </ul>
       <ul className="flex-row flex justify-evenly flex-wrap">
-        {skills &&
-          Object.keys(skills).map((skill: any, index: number) => (
-            <Skill {...skills[skill]} key={`${index}`} />
+        {renderSkill &&
+          renderSkill.map((skill: fuseSkillInterface, index: number) => (
+            <Skill {...skill} key={`${index}`} />
           ))}
       </ul>
     </section>
