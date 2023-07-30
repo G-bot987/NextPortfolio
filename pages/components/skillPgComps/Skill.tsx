fbef3ed3d1c5b08d8cfe1cc4620e2669fd620b73
gbot987 <ghobs987@gmail.com>
2023-07-30 20:54:09 +0100

    destructuring fuse search data

--- pages/components/skillPgComps/Skill.tsx
+++ pages/components/skillPgComps/Skill.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import styles from "../../styles/Home.module.css";
-import { SkillInterface } from "../../../interface/technologies.interface";
+import { fuseSkillInterface } from "../../../interface/technologies.interface";
 
-export default function Skill(props: SkillInterface) {
-  const { name } = props;
+export default function Skill(props: fuseSkillInterface) {
+  const { item } = props ?? {};
+  const { name } = item ?? {};
+
   return <li className="max-w-xs my-3 mx-3 text-white font-tektur">{name}</li>;
 }
