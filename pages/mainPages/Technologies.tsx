1ecb9408bf3705777ed37d0fcb9d917f21ce3745
gbot987 <ghobs987@gmail.com>
2023-07-30 21:58:44 +0100

    improved ux for tecs

--- pages/mainPages/Technologies.tsx
+++ pages/mainPages/Technologies.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Fuse from "fuse.js";
 import styles from "../../styles/Home.module.css";
@@ -9,15 +9,14 @@ import {
   fuseSkillInterface,
 } from "../../interface/technologies.interface";
 import Skill from "../components/skillPgComps/Skill";
+import AllSkills from "../components/skillPgComps/AllSkills";
 
 export default function Technologies(props: TechnologiesInterface) {
   const { skills, header, skillgroups } = props;
 
-  const [tecSearchInput, setTecSearchInput]: any = useState("");
   const [renderSkill, setRenderSKill]: any = useState([]);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTecSearchInput(event.target.value);
     filterTechnology(event.target.value);
   };
 
@@ -26,34 +25,46 @@ export default function Technologies(props: TechnologiesInterface) {
       includeScore: true,
       keys: ["name"],
     };
-    const fuse = new Fuse(skills, options);
-    const foundSkills = fuse.search(`${tecSearchInput}`, { limit: 3 });
+    if (tecSearchInput.trim() !== "") {
+      const fuse = new Fuse(skills, options);
+      const foundSkills = fuse.search(`${tecSearchInput}`, { limit: 3 });
 
-    setRenderSKill(foundSkills);
+      return setRenderSKill(foundSkills);
+    } else {
+      return setRenderSKill(skills);
+    }
   };
-
+  useEffect(() => setRenderSKill(skills), []);
   return (
-    <section className="flex flex-col text-white font-tektur">
-      <ul className="flex flex-col items-center max-md:hidden ">
-        <h2 className="font-bd-retrocentric uppercase underline">{header}</h2>
-        <li className="flex flex-row"></li>
-        <li className="max-w-fit rounded-full border-2 border-white p-2 my-2 uppercase">
-          <label htmlFor="tec-search">Skill Search:</label>
-          <input
-            type="search"
-            id="tec-search"
-            name="tec"
-            value={tecSearchInput}
-            onChange={handleInputChange}
-          />
-        </li>
-      </ul>
-      <ul className="flex-row flex justify-evenly flex-wrap">
-        {renderSkill &&
-          renderSkill.map((skill: fuseSkillInterface, index: number) => (
-            <Skill {...skill} key={`${index}`} />
-          ))}
-      </ul>
+    <section className="px-8">
+      <article className="flex flex-col text-white font-tektur  px-10  py-4 justify-between items-center border-2 border-white rounded-full">
+        <ul className="flex flex-col items-center max-md:hidden  rounded-full   ">
+          <h2 className="font-bd-retrocentric uppercase underline">{header}</h2>
+          <li className="flex flex-row"></li>
+          <li className="max-w-fit  p-2 my-2 uppercase">
+            <label htmlFor="tec-search">Competencies Search:</label>
+            <input
+              type="search"
+              id="tec-search"
+              name="tec"
+              onChange={handleInputChange}
+            />
+          </li>
+        </ul>
+        <ul className="flex-row flex justify-evenly flex-wrap   ">
+          {renderSkill &&
+            renderSkill.length >= 3 &&
+            renderSkill.map((skill: SkillInterface, index: number) => (
+              <AllSkills {...skill} key={`${index}`} />
+            ))}
+
+          {renderSkill &&
+            3 >= renderSkill.length &&
+            renderSkill.map((skill: fuseSkillInterface, index: number) => (
+              <Skill {...skill} key={`${index}`} />
+            ))}
+        </ul>
+      </article>
     </section>
   );
 }
