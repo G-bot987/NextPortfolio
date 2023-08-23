79c8c60d249ecb9913683587369e559bb851f760
gbot987 <ghobs987@gmail.com>
2023-08-24 00:05:57 +0100

    render selected white hexagon

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ export default function App() {
 
   const [open, SetOpen] = useState(false);
   const [hover, setHover] = useState(false);
-  const [currentPage, setCurrentPage] = useState("Home");
+  const [currentPage, setCurrentPage] = useState("Technologies");
   const ref = useRef<null | HTMLDivElement>(null);
 
   const handleMouseOver = () => {
