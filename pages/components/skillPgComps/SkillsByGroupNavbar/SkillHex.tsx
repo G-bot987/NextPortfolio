5069e23e9779abbba6073417770cbbabe9de2ad4
gbot987 <ghobs987@gmail.com>
2023-09-19 22:44:48 +0100

    short term fix for open skills hex

--- pages/components/skillPgComps/SkillsByGroupNavbar/SkillHex.tsx
+++ pages/components/skillPgComps/SkillsByGroupNavbar/SkillHex.tsx
@@ -19,7 +19,8 @@ export default function SkillHex(Props: SkillInterface & { index: number }) {
       onMouseLeave={handleMouseLeave}
       className={` h-[95px] flex flex-col items-center justify-center flex ${
         (index + 1) % 3 == 0 ? `mx-[47px]` : ``
-      } ${hover ? "w-[90px]" : "w-[94px]"} `}
+      } 
+      ${hover ? "w-[94px]" : "w-[94px]"} `}
     >
       <div className="pr-2 pb-1">
         <div
@@ -31,7 +32,7 @@ export default function SkillHex(Props: SkillInterface & { index: number }) {
         <div className="h-0 w-0 border-t-12 border-l-6 border-r-6 border-white"></div>
       </div>
       <button
-        className={`min-w-[98px] ${hover ? "bg-white w-[120px]" : ""}`}
+        className={`min-w-[98px] ${hover ? "" : ""}`}
         // onClick={(event) => {
         //   event.preventDefault();
         //   filterSkillGroups(keyProp);
@@ -43,7 +44,7 @@ export default function SkillHex(Props: SkillInterface & { index: number }) {
           <div className={`h-[48px] w-[2px] bg-white ${styles.glow}`} />
           <p
             className={`max-w-min text-[10px] uppercase text-white text-center ${
-              hover ? " text-black" : "text-white"
+              hover ? " text-white" : "text-white"
             }`}
           >
             {name}
@@ -54,11 +55,11 @@ export default function SkillHex(Props: SkillInterface & { index: number }) {
       <div className="pt-1 pl-6">
         <div
           className={`h-[2px] w-[70px] bg-white transform -rotate-45 translate-x-3 translate-y-4 ${styles.glow}`}
-        ></div>
+        />
         <div
           className={`h-[2px] w-[69px] bg-white transform rotate-45  translate-y-4 -translate-x-9 ${styles.glow}`}
-        ></div>
-      </div>{" "}
+        />
+      </div>
     </li>
   );
 }
